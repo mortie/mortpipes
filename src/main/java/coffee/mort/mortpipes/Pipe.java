0a7a6834fc71b6830c17826442b07627b9205dfe
mort <martid0311@gmail.com>
2015-11-12 21:46:13 +0100

    model changes

--- src/main/java/coffee/mort/mortpipes/Pipe.java
+++ src/main/java/coffee/mort/mortpipes/Pipe.java
@@ -7,32 +7,36 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.BlockPos;
 import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.block.material.Material;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.IBlockAccess;
 
 class Pipe extends MortBlockContainer {
-	public static final PropertyBool NORTH = PropertyBool.create("north");
-	public static final PropertyBool EAST = PropertyBool.create("east");
-	public static final PropertyBool SOUTH = PropertyBool.create("south");
-	public static final PropertyBool WEST = PropertyBool.create("west");
-	public static final PropertyBool UP = PropertyBool.create("up");
-	public static final PropertyBool DOWN = PropertyBool.create("down");
+	public static final PropertyEnum NORTH = PropertyEnum.create("north", Pipe.AttachType.class);
+	public static final PropertyEnum EAST = PropertyEnum.create("east", Pipe.AttachType.class);
+	public static final PropertyEnum SOUTH = PropertyEnum.create("south", Pipe.AttachType.class);
+	public static final PropertyEnum WEST = PropertyEnum.create("west", Pipe.AttachType.class);
+	public static final PropertyEnum UP = PropertyEnum.create("up", Pipe.AttachType.class);
+	public static final PropertyEnum DOWN = PropertyEnum.create("down", Pipe.AttachType.class);
+	public static final PropertyBool NOBLOCK = PropertyBool.create("noblock");
 
 	public Pipe(String name) {
 		super(Material.iron, name);
 
 		this.setDefaultState(this.blockState.getBaseState()
-			.withProperty(NORTH, false)
-			.withProperty(EAST, false)
-			.withProperty(SOUTH, false)
-			.withProperty(WEST, false)
-			.withProperty(UP, false)
-			.withProperty(DOWN, false));
+			.withProperty(NORTH, AttachType.NONE)
+			.withProperty(EAST, AttachType.NONE)
+			.withProperty(SOUTH, AttachType.NONE)
+			.withProperty(WEST, AttachType.NONE)
+			.withProperty(UP, AttachType.NONE)
+			.withProperty(DOWN, AttachType.NONE)
+			.withProperty(NOBLOCK, false));
 
 		setHitbox(this.blockState.getBaseState());
 	}
@@ -50,21 +54,76 @@ class Pipe extends MortBlockContainer {
 	}
 
 	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-		state = state.withProperty(NORTH, this.canPipeConnect(worldIn, pos, EnumFacing.NORTH));
-		state = state.withProperty(EAST, this.canPipeConnect(worldIn, pos, EnumFacing.EAST));
-		state = state.withProperty(SOUTH, this.canPipeConnect(worldIn, pos, EnumFacing.SOUTH));
-		state = state.withProperty(WEST, this.canPipeConnect(worldIn, pos, EnumFacing.WEST));
-		state = state.withProperty(UP, this.canPipeConnect(worldIn, pos, EnumFacing.UP));
-		state = state.withProperty(DOWN, this.canPipeConnect(worldIn, pos, EnumFacing.DOWN));
+		AttachType north = canPipeConnect(worldIn, pos, EnumFacing.NORTH);
+		AttachType east = canPipeConnect(worldIn, pos, EnumFacing.EAST);
+		AttachType south = canPipeConnect(worldIn, pos, EnumFacing.SOUTH);
+		AttachType west = canPipeConnect(worldIn, pos, EnumFacing.WEST);
+		AttachType up = canPipeConnect(worldIn, pos, EnumFacing.UP);
+		AttachType down = canPipeConnect(worldIn, pos, EnumFacing.DOWN);
+
+		state = state.withProperty(NORTH, north);
+		state = state.withProperty(EAST, east);
+		state = state.withProperty(SOUTH, south);
+		state = state.withProperty(WEST, west);
+		state = state.withProperty(UP, up);
+		state = state.withProperty(DOWN, down);
+
+		if (
+			(
+				(north != AttachType.NONE && south != AttachType.NONE) &&
+				(east == AttachType.NONE && west == AttachType.NONE) &&
+				(up == AttachType.NONE && down == AttachType.NONE)
+			) ||
+			(
+				(north == AttachType.NONE && south == AttachType.NONE) &&
+				(east != AttachType.NONE && west != AttachType.NONE) &&
+				(up == AttachType.NONE && down == AttachType.NONE)
+			) ||
+			(
+				(north == AttachType.NONE && south == AttachType.NONE) &&
+				(east == AttachType.NONE && west == AttachType.NONE) &&
+				(up != AttachType.NONE && down != AttachType.NONE)
+			)
+		) {
+			state = state.withProperty(NOBLOCK, true);
+		} else {
+			state = state.withProperty(NOBLOCK, false);
+		}
 
 		return state;
 	}
 
-	private boolean canPipeConnect(IBlockAccess worldIn, BlockPos pos, EnumFacing direction) {
+	private AttachType canPipeConnect(IBlockAccess worldIn, BlockPos pos, EnumFacing direction) {
 		BlockPos offPos = pos.offset(direction);
 		Block block = worldIn.getBlockState(offPos).getBlock();
 
-		return (block instanceof Pipe);
+		if (block instanceof Pipe)
+			return AttachType.PIPE;
+
+		TileEntity ent = worldIn.getTileEntity(offPos);
+		if (ent != null && ent instanceof IInventory)
+			return AttachType.INVENTORY;
+
+		return AttachType.NONE;
+	}
+
+	public static enum AttachType implements IStringSerializable {
+		PIPE("pipe"),
+		INVENTORY("inventory"),
+		NONE("none");
+		private final String name;
+
+		private AttachType(String name) {
+			this.name = name;
+		}
+
+		public String toString() {
+			return name;
+		}
+
+		public String getName() {
+			return name;
+		}
 	}
 
 	@Override
@@ -79,7 +138,7 @@ class Pipe extends MortBlockContainer {
 
 	@Override
 	protected BlockState createBlockState() {
-		return new BlockState(this, new IProperty[] {NORTH, EAST, SOUTH, WEST, UP, DOWN});
+		return new BlockState(this, new IProperty[] {NORTH, EAST, SOUTH, WEST, UP, DOWN, NOBLOCK});
 	}
 
 	@Override
